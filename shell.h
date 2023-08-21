4b68eaa2b1418142a9c7dbac53f12a5cc2d08de8
hidaya <hidaya.regragui@gmail.com>
2023-08-21 09:49:05 -0700

    shell.h, fix betty

--- shell.h
+++ shell.h
@@ -1,1 +1,17 @@
+#ifndef SHELL_H
+#define SHELL_H
+
 #include <stdio.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <sys/types.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#define BUFSIZE 1024
+#define DELIMITER " \t\r\n\a"
+#endif
--- simple_shell.c
+++ simple_shell.c
@@ -1,14 +1,5 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <dirent.h>
+#include "shell.h"
 
-#define MAX_INPUT_LENGTH 100
 /**
  * main - Entry point of the program
  *
@@ -16,61 +7,59 @@
  */
 int main(void)
 {
-    char input[MAX_INPUT_LENGTH];
-    char *args[MAX_INPUT_LENGTH / 2 + 1];  // Command and arguments
-    pid_t child_pid;
-    int status;
+	char input[BUFSIZE];
+	char *args[BUFSIZE / 2 + 1];
+	pid_t child_pid;
+	int status, num_args;
+	char *token;
+	
+	while (1)
+	{
+		write(1, "$cisfun$ ", 9);
+		/* Read user input*/
+		if (fgets(input, BUFSIZE, stdin) == NULL)
+		{
+			printf("\n");
+			break;
+		}
 
-    while (1)
-    {
-        printf("#cisfun$ ");
-        fflush(stdout);
+		/* Remove newline character from input*/
+		input[strcspn(input, "\n")] = '\0';
 
-        // Read user input
-        if (fgets(input, MAX_INPUT_LENGTH, stdin) == NULL)
-        {
-            // Handle end of file (Ctrl+D)
-            printf("\n");
-            break;
-        }
+		/* Tokenize input into arguments*/
+		token = strtok(input, DELIMITER);
 
-        // Remove newline character from input
-        input[strcspn(input, "\n")] = '\0';
+		num_args = 0;
+		while (token)
+		{
+			args[num_args] = token;
+			num_args++;
+			token = strtok(NULL, DELIMITER);
+		}
+		args[num_args] = NULL;
+		
+		child_pid = fork();
 
-        // Tokenize input into arguments
-        int num_args = 0;
-        char *token = strtok(input, " ");
-        while (token != NULL)
-        {
-            args[num_args] = token;
-            num_args++;
-            token = strtok(NULL, " ");
-        }
-        args[num_args] = NULL;
+		if (child_pid == -1)
+		{
+			perror("Error:");
+			return (1);
+		}
+		if (child_pid == 0)
+		{
+			/* Child process*/
+			if (execve(args[0], args, NULL) == -1)
+			{
+				perror("Error in execve:");
+				return (1);
+			}
+		}
+		else
+		{
+			/* Parent process*/
+			wait(&status);
+		}
+	}
 
-        child_pid = fork();
-
-        if (child_pid == -1)
-        {
-            perror("Error:");
-            return 1;
-        }
-
-        if (child_pid == 0)
-        {
-            // Child process
-            if (execve(args[0], args, NULL) == -1)
-            {
-                perror("Error in execve:");
-                return 1;
-            }
-        }
-        else
-        {
-            // Parent process
-            wait(&status);
-        }
-    }
-
-    return 0;
+	return (0);
 }
